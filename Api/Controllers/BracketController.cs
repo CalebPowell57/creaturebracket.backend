f21659a1ccf47b6850b3185a7ee8df4edfc58749
CalebPowell57 <calebpowell57@yahoo.com>
2023-02-05 01:24:49 -0600

    so

--- Api/Controllers/BracketController.cs
+++ Api/Controllers/BracketController.cs
@@ -27,12 +27,27 @@ namespace Api.Controllers
             return bracket;
         }
 
-        [HttpGet("bracket/{bracketId}")]
+        [HttpGet("standings/{bracketId}")]
         public async Task<IEnumerable<StandingsDto>> GetStandings([FromRoute] long bracketId)
         {
             var standings = await _bracketService.GetStandings(bracketId);
 
             return standings;
+        }
+
+        [HttpPost]
+        public async Task<bool> Post([FromBody] Model.Dto.Post.BracketDto bracket)
+        {
+            for (var index = 0; index < bracket.CreatureCount / 2; index++)
+            {
+
+            }
+
+            _bracketService.Upsert(bracket);
+
+            await _bracketService.SaveChanges();
+
+            return true;
         }
     }
 }
---
+++ Model/Dto/Post/BracketDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Model.Dto.Post
+{
+    public class BracketDto
+    {
+        public long CreatureCount { get; set; }
+        public string Name { get; set; }
+    }
+}
